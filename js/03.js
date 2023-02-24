1fe464075de90a5016586e2a68ed61b5ee3e6bf7
LuisMarioSolano <luismariosga@gmail.com>
2023-02-24 13:41:53 -0600

    Update js/03.js: variables const

---
+++ js/03.js
@@ -0,0 +1,22 @@
+// Variables con const
+
+const producto = 'Audifonos Gamer';  //Iniciar variable y asignar valor
+
+let disponible = true //No se puede crear variables sin valor con const
+
+// producto = true; //Reasignando el valor de la variable
+
+disponible = true;
+
+const producto1 = 'Computadora',
+    disponible1 = true,
+    categoria = 'Computadora';
+
+// const 1disponible;
+const _disponible = true;
+
+//Estilos para las variables
+const nombre_producto = 'Monitor HD'; //underscore
+const nombreProducto = 'Monitor HD'; //Camelcase
+const NombreProducto = 'Monitor HD'; // Pascal Case
+const nombreproducto = 'Monitor HD';
