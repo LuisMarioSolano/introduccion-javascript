e152b2f4840d347a8328a6852bcedbb1a371fcab
LuisMarioSolano <luismariosga@gmail.com>
2023-02-24 13:36:30 -0600

    Update js/02.js: variables let

--- js/02.js
+++ js/02.js
@@ -1,2 +1,23 @@
 // Variables con let
+// Variables con var y let son lo mismo, solo que let es el usado actualmente
 
+let producto = 'Audifonos Gamer';  //Iniciar variable y asignar valor
+
+let disponible; //Iniciar variable sin valor asignado
+
+// producto = true; //Reasignando el valor de la variable
+
+disponible = true;
+
+let producto1 = 'Computadora',
+    disponible1 = true,
+    categoria = 'Computadora';
+
+// let 1disponible;
+let _disponible;
+
+//Estilos para las variables
+let nombre_producto = 'Monitor HD'; //underscore
+let nombreProducto = 'Monitor HD'; //Camelcase
+let NombreProducto = 'Monitor HD'; // Pascal Case
+let nombreproducto = 'Monitor HD';
