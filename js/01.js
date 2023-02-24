7aba86a5b9b3ef6d3fca2b617de2c255d6821db0
LuisMarioSolano <luismariosga@gmail.com>
2023-02-24 13:31:36 -0600

    Update js/01.js: Variables JS

---
+++ js/01.js
@@ -0,0 +1,24 @@
+// Variables 
+
+var producto = 'Audifonos Gamer';  //Iniciar variable y asignar valor
+
+var disponible; //Iniciar variable sin valor asignado
+
+// producto = true; //Reasignando el valor de la variable
+
+disponible = true;
+
+var producto1 = 'Computadora',
+    disponible1 = true,
+    categoria = 'Computadora';
+
+// var 1disponible;
+var _disponible;
+
+//Estilos para las variables
+var nombre_producto = 'Monitor HD'; //underscore
+var nombreProducto = 'Monitor HD'; //Camelcase
+var NombreProducto = 'Monitor HD'; // Pascal Case
+var nombreproducto = 'Monitor HD';
+
+console.log(producto);
