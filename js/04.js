0a62a82c3be90965f3e1e271a1c3cd3fe137b79d
LuisMarioSolano <luismariosga@gmail.com>
2023-02-27 14:51:03 -0600

    Update js/04.js: type of strings

--- js/04.js
+++ js/04.js
@@ -1,2 +1,6 @@
 const producto = "Monitor de 20 pulgadas";
+const producto2= String('Monitor 30 Pulgdas');
+const producto3= new String('Monitor 50 Pulgadas')
 console.log(producto)
+console.log(producto2)
+console.log(producto3)
