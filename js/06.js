f2e4e190dceff6227a3f59161145a70481895128
LuisMarioSolano <luismariosga@gmail.com>
2023-02-27 16:04:47 -0600

    Update js/06.js: Objeto Math

---
+++ js/06.js
@@ -0,0 +1,16 @@
+// Objeto Math
+
+let resultado;
+
+resultado = Math.PI;
+resultado = Math.round(2.5);
+resultado = Math.ceil(2.1); //Ceil siempre redondea hacia arriba
+resultado = Math.floor(2.9); //Floor siempre redondea hacia abajo
+resultado = Math.sqrt(144); //Raíz cuadrada
+resultado = Math.abs(-200); //Poner en positivo
+resultado = Math.min(3, 5, 1, 8, 2, 10); //Regresa número más chico
+resultado = Math.max(3, 5, 1, 8, 2, 10); //Regresa número más grande
+resultado = Math.random(); //Numero aleatorio (siempre es menor a 1)
+resultado = Math.floor(Math.random() * 30); //Número aleatorio
+
+console.log(resultado);
