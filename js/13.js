8cae855ed76a25f09992a155d80e9291c01a730b
LuisMarioSolano <luismariosga@gmail.com>
2023-02-28 16:29:17 -0600

    Update js/13.js: Unir objetos sin modificarlos

--- js/13.js
+++ js/13.js
@@ -4,3 +4,14 @@ const producto = {
     precio : 300,
     disponible : true,
 }
+
+const medidas = {
+    peso: '1kg',
+    medida: '1m'
+}
+
+const nuevoProducto = {...producto, ...medidas};
+
+
+console.log(producto);
+console.log(nuevoProducto);
