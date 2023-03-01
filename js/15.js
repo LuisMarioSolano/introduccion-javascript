e59b8964756478ee58a6b735ed6a1760ee088a7a
LuisMarioSolano <luismariosga@gmail.com>
2023-03-01 14:08:26 -0600

    Update js/15.js: Mas metodos de Arrays

---
+++ js/15.js
@@ -0,0 +1,44 @@
+// Array Methods
+
+const meses = ["Enero", "Feberero", "Marzo", "Abril", "Mayo"];
+
+const carrito = [
+    {nombre: 'Monitor 20 Pulgadas', precio: 500},
+    {nombre: 'Television de 50 Pulgadas', precio: 700},
+    {nombre: 'Tablet', precio: 300},
+    {nombre: 'Audifonos', precio: 200},
+    {nombre: 'Teclado', precio: 50},
+    {nombre: 'Celular', precio: 500},
+    {nombre: 'Bocinas', precio: 300},
+    {nombre: 'Laptop', precio: 800},
+];
+
+// forEach
+meses.forEach(function(mes){
+    if(mes == 'Marzo'){
+        console.log('Marzo si existe')
+    }
+});
+
+// Includes
+let resultado = meses.includes('Marzo');
+
+// Some ideal para arreglo de objetos
+resultado = carrito.some(function(producto){
+    return producto.nombre === 'Celular'
+})
+
+// Reduce
+resultado = carrito.reduce(function(total, producto) { 
+    return total + producto.precio
+}, 0)
+
+
+
+// Filter
+resultado = carrito.filter(function(producto){
+    return producto.nombre !== 'Celular'
+});
+
+
+console.log(resultado);
