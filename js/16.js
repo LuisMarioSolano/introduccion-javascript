5bc88d9e3a4aaf1262b1dc732ce3345341cee3a9
LuisMarioSolano <luismariosga@gmail.com>
2023-03-01 14:39:11 -0600

    Update js/16.js: Diferencias entre tipos de Funciones

--- js/16.js
+++ js/16.js
@@ -2,16 +2,16 @@
 
 // Declaración de funcion
 
+sumar();
 function sumar() {
     console.log(10*10);
 }
-sumar();
 
 // Expreción de la función
+sumar2();
 const sumar2 = function() {
     console.log(3 + 3);
 }
-sumar2();
 
 // IIFE (Protege variables de otros archivos)
 (function() {
