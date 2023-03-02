556cbb4db57c36aaa370752824ea8c5f70177065
LuisMarioSolano <luismariosga@gmail.com>
2023-03-02 13:35:42 -0600

    Create js/20.js: Métodos de propiedad

---
+++ js/20.js
@@ -0,0 +1,26 @@
+// Metodos de propiedad
+
+const reproductor = {
+    reproducir : function(id) {
+        console.log(`Reproduciendo Canción con el ID: ${id}`)
+    },
+    pausar : function() {
+        console.log(`Pausando...`)
+    },
+    crearPlaylist : function(nombre) {
+        console.log(`Creando la playlist: ${nombre}`)
+    },
+    reproducirPlaylist : function(nombre) {
+        console.log(`Reproduciendo playlist: ${nombre}`)
+    }
+}
+
+reproductor.borrarCancion = function(id){
+    console.log(`Eliminando la cancion ${id}`)
+}
+
+reproductor.reproducir(3840);
+reproductor.pausar();
+reproductor.borrarCancion(20);
+reproductor.crearPlaylist('Heavy Metal');
+reproductor.reproducirPlaylist('Heavy Metal');
