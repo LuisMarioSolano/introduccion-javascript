283ae71c25ed4d005f9d86cfa18d297977cd4fb1
LuisMarioSolano <luismariosga@gmail.com>
2023-03-03 15:36:36 -0600

    Update js/27.js: Prototype

--- js/27.js
+++ js/27.js
@@ -9,19 +9,33 @@ const producto = {
 
 
 // Object Constructor
+function Cliente(nombre, apellido) {
+    this.nombre = nombre;
+    this.apellido= apellido;
+}
+Cliente.prototype.formatearCliente = function(producto) {
+    return `El Cliente ${this.nombre} ${this.apellido}`;
+}
+
 function Producto (nombre, precio, disponible) {
     this.nombre = nombre;
     this.precio = precio;
     this.disponible = disponible;
 }
-const producto2 = new Producto('Monito Curvo de 49"', 800, true);
-const producto3 = new Producto('Laptop', 300, false);
-const producto4 = new Producto('Tablet', 500, true);
-const producto5 = new Producto('Teclado', 100, true);
-const producto6 = new Producto('Mouse', 50, false);
 
-console.log(producto2);
-console.log(producto3);
-console.log(producto4);
-console.log(producto5);
-console.log(producto6);
+// Crear funciones que solo se utilizan en un objeto en especifico 
+Producto.prototype.formatearProducto = function() {
+    return `El Producto ${this.nombre} tiene un precio de: $${this.precio}`;
+}
+
+const producto2 = new Producto('Monitor Curvo de 49"', 800, true);
+const producto3 = new Producto('Laptop', 300, false);
+const cliente = new Cliente('Mario', 'Solano');
+
+
+console.log(producto2)
+console.log(producto2.formatearProducto());
+console.log(producto3.formatearProducto());
+
+
+
