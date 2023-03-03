1646a6e8c2465d426aefdd7e9bd5a9a8b64a9ae6
LuisMarioSolano <luismariosga@gmail.com>
2023-03-03 16:25:01 -0600

    Update js/28.js: POO - Herencia

--- js/28.js
+++ js/28.js
@@ -7,12 +7,28 @@ class Producto {
     }
 
     formatearProducto() {
-        return `El Producto ${this.nombre} tiene un precio de: $${this.precio}`;
+        return `El producto ${this.nombre} tiene un precio de $${this.precio}`;
     }
 }
 
 const producto2 = new Producto('Monitor Curvo de 49"', 800, true);
 const producto3 = new Producto('Laptop', 300, false);
 
-console.log(producto2);
-console.log(producto3);
+
+// Herencia 
+
+class Libro extends Producto {
+        constructor(nombre, precio, isbn) {
+            super(nombre, precio);
+            this.isbn = isbn;
+        }
+
+        formatearProducto() {
+            return `${super.formatearProducto()} y su ISBN es ${this.isbn}`;
+        }
+}
+
+const libro = new Libro('JavaScript la Revolución', 120, '6516161506516162191651');
+console.log(libro.formatearProducto());
+
+console.log(producto2.formatearProducto());
