1f4336e16e0628500b5e2db6bdc8afe935354d4e
LuisMarioSolano <luismariosga@gmail.com>
2023-03-03 16:32:03 -0600

    Create js/29.js: try catch

---
+++ js/29.js
@@ -0,0 +1,15 @@
+// try catch
+
+const numero1 = 20;
+const numero3 = 30;
+
+console.log(numero1);
+
+// Errores sin parar la ejecución del código
+try {
+    console.log(numero2);
+} catch (error) {
+    console.log(error);
+}
+
+console.log(numero3);
