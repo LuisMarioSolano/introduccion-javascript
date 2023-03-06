cdc2d0be3077270093207b042748b7070b587e0c
LuisMarioSolano <luismariosga@gmail.com>
2023-03-06 13:38:19 -0600

    Create js/31.js: Notification API

---
+++ js/31.js
@@ -0,0 +1,14 @@
+// Notification API
+
+const boton = document.querySelector('#boton');
+boton.addEventListener('click', () => {
+    Notification.requestPermission()
+        .then(resultado => console.log(`El resultado es: ${resultado}`))
+});
+
+if (Notification.permission == 'granted') {
+    new Notification ('Esta es una notificación', {
+        icon: 'img/icono_2.png',
+        body: 'HTML'
+    })
+}
