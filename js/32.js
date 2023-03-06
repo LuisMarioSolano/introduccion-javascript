8e845620e36bf4471755010338d963deb1b57993
LuisMarioSolano <luismariosga@gmail.com>
2023-03-06 13:54:19 -0600

    Create js/32.js: Sync/Await

---
+++ js/32.js
@@ -0,0 +1,26 @@
+// Async / Await
+
+function descargarNuevosClientes() {
+    return new Promise(resolve => {
+        console.log('Descargando clientes... espere..');
+
+        setTimeout(() => {
+            resolve('Los Clientes fueron Descargados');
+        }, 5000)
+
+
+    });
+}
+
+async function app() {
+    try {
+        const resultado = await descargarNuevosClientes();
+        console.log(resultado);
+    } catch (error) {
+        console.log(error);
+    }
+
+}
+
+app();
+
